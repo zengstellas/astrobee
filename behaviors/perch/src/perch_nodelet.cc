0c62fe9dba14dcedaf2673b5e885bff954922326
Stella Zeng <zengstellas@gmail.com>
2022-07-15 14:35:24 -0400

    Give more time for commands

--- behaviors/perch/src/perch_nodelet.cc
+++ behaviors/perch/src/perch_nodelet.cc
@@ -157,7 +157,7 @@ class PerchNodelet : public ff_util::FreeFlyerNodelet {
     fsm_.Add(STATE::PERCHING_SWITCHING_TO_HR_LOC,
       SWITCH_SUCCESS, [this](FSM::Event const& event) -> FSM::State {
         SaveApproachPose();
-        ros::Duration(5).sleep(); //sleep for 5 seconds
+        ros::Duration(10).sleep(); //sleep for 10 seconds
         Move(APPROACH_POSE, ff_msgs::MotionGoal::NOMINAL);
         return STATE::PERCHING_MOVING_TO_APPROACH_POSE;
       });
