4135a4c4f5ff65eda3db42718f5da1bb869bfeb1
Stella Zeng <zengstellas@gmail.com>
2022-08-01 15:30:02 -0400

    Correcting variable types for body/sensor conversion

--- simulation/src/gazebo_sensor_plugin_depth_odom/gazebo_sensor_plugin_depth_odom.cc
+++ simulation/src/gazebo_sensor_plugin_depth_odom/gazebo_sensor_plugin_depth_odom.cc
@@ -171,7 +171,7 @@ class GazeboSensorPluginDepthOdom : public FreeFlyerSensorPlugin {
     tf2::toMsg(diff_, sensor_pose_msg_);
 
     // Calculate pose relative to body from sensor
-    tf2::toMsg(body_T_perch_cam_ * diff_ * body_T_perch_cam_.inverse(), body_pose_msg_);
+    tf2::toMsg(body_T_perch_cam_ * tf2::transformToEigen(sensor_pose_msg_) * body_T_perch_cam_.inverse(), body_pose_msg_);
 
     msg_do_.odometry.sensor_F_source_T_target.pose = sensor_pose_msg_;
     msg_do_.odometry.body_F_source_T_target.pose = body_pose_msg_;
